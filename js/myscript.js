baaaf7cdf7020b761dfec7b46a678269c69404d2
AlessioAbbati <alex_117@live.it>
2023-04-04 18:19:02 +0200

    add surname and favcolor

--- js/myscript.js
+++ js/myscript.js
@@ -3,3 +3,17 @@ const userName = prompt ('inserisci il tuo nome');
 const elespan = document.getElementById('name');
 
 elespan.innerHTML = userName;
+
+
+const usersurName = prompt ('inserisci il tuo cognonome');
+
+const elespan2 = document.getElementById('surname');
+
+elespan2.innerHTML = usersurName;
+
+
+const favColor = prompt ('inserisci il tuo colore preferito');
+
+const elespan3 = document.getElementById('favourite-color');
+
+elespan3.innerHTML = favColor;
